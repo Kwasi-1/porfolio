629ba42938b73e3f5ce5129dfa6e33e47081452a
kwasi <affulnana04@gmail.com>
2025-02-20 22:39:49 +0000

    change margin and position of this section

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const MainContent = ({ title, paragraphs }) => {
+const MainContent = ({ title, paragraphs, paddingTop }) => {
   return (
-    <div className="flex flex-col justify-end w-[30%] py-24 mr-[5%] font-body">
+    <div className={`flex flex-col fixed h-screen w-[30%] py-24 mr-[5%] font-body ${paddingTop} px-[3%]`}>
       <div className="space-y-4">
         <h1 className="text-base font-[500] tracking-widest uppercase">{title}</h1>
         <div className="text-[13px] leading-6 font-[300] tracking-wider">
