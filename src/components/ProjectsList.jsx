9c087fd7421aee1623dfee823ebbb1f161d6e0e0
kwasi <affulnana04@gmail.com>
2025-03-02 21:58:01 +0000

    add project links

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const ProjectsList = () => {
   const projects = [
-    { name: 'Amazon', type: 'Frontend Development' },
-    { name: 'SwissBik', type: 'Internationalization (i18n)', new: true },
-    { name: 'AutoChef', type: 'Frontend Development', new: true },
-    { name: 'Dept.CS', type: 'Frontend Development' },
+    { name: 'Amazon', type: 'Frontend Development', url: 'https://www.amazon.com' },
+    { name: 'SwissBik', type: 'Internationalization (i18n)', url: 'https://www.swissbik.com', new: true },
+    { name: 'AutoChef', type: 'Frontend Development', url: 'https://www.autochef.com', new: true },
+    { name: 'Dept.CS', type: 'Frontend Development', url: 'https://www.deptcs.com' },
   ];
 
   return (
     <div className="relative flex flex-col space-y-4 sm:space-y-6 text-[10vw] sm:text-[8vw] lg:text-[7vw] w-full lg:w-[65%] lg:ml-[45%] lg:py-[10%] px-[10%] lg:px-0 font-light font-custom leading-[1.2] lg:leading-[1] mx-auto ">
       {projects.map((project, index) => (
-        <Link
+        <a
           key={index}
-          to={`/work/${project.name.toLowerCase()}`} // Add a proper link path
+          href={project.url} // Use the external URL here
+          target="_blank" // Open link in a new tab
+          rel="noopener noreferrer" // Security best practice for external links
           className="justify-between items-center w-fit hover:italic hover:translate-x-[5px] transform ease-in-out duration-300"
         >
           <h2 className="uppercase">{project.name}</h2>
@@ -28,7 +29,7 @@ const ProjectsList = () => {
               </span>
             )}
           </div>
-        </Link>
+        </a>
       ))}
     </div>
   );
